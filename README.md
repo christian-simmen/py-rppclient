af2396c71299ad45e95907cb689bb60869478fab
Christian Simmen <simmen@denic.de>
2025-03-18 12:35:25 +0100

    add interactive and verbose mode

--- README.md
+++ README.md
@@ -1,29 +1,44 @@
 # Python RPP Client
 
+RPP is still work in progress. For more information about the current progress please see:
+
+* (IETF WG)[https://datatracker.ietf.org/group/rpp/about/]
+* (RPP WG Github Organization)[https://github.com/ietf-wg-rpp]
+* (RPP WG Wiki)[https://wiki.ietf.org/en/group/rpp]
+
+This program was part of the (IETF 122 Hackathon)[https://wiki.ietf.org/en/group/rpp/rpp-hackathon]
+
+## What to expect
+* GET a domain object from a server
+*
+
+## What NOT to expect
+For RPP is not fully defined most of the (requirements)[https://github.com/SIDN/ietf-wg-rpp-charter/blob/main/requirements.md] are not implemented yet.
+
 ## Usage
 
 ```bash
-usage: py-rppclient.py [-h] -s SERVER [-d DOMAIN] {fetch,create,info,check,update,delete}
+usage: py-rppclient.py [-h] [-s SERVER] [-d DOMAIN] [-i] [-v] {create,info,check,update,delete}
 
 Python RPP Client
 
 positional arguments:
-  {fetch,create,info,check,update,delete}
+  {create,info,check,update,delete}
                         Operation to perform
 
 options:
   -h, --help            show this help message and exit
   -s, --server SERVER   Base URL of the Server API
-  -d, --domain DOMAIN   Domain (required for info, check, update, and delete operations)
+  -d, --domain DOMAIN   Domainname
+  -i, --interactive     Interactive template expansion
+  -v, --verbose         Verbose mode
+
 ```
 
 ## Examples
 ```bash
-# fetch existing domains
-./py-rppclient.py fetch --base_url "http://127.0.0.1:8000"
-# fetch,create,get,update,delete
 # create example.de
-./py-rppclient.py create --base_url "http://127.0.0.1:8000" --domain_id example.de
+./py-rppclient.py create --base_url "http://127.0.0.1:8000" --domain_id example.de -i
 
 # Domain INFO example.de
 ./py-rppclient.py info --base_url "http://127.0.0.1:8000" --domain_id example.de
--- py-rppclient.py
+++ py-rppclient.py
@@ -1,11 +1,94 @@
 #!/usr/bin/python3
 
-
 import requests
-#import argparse
+import json
 from argparse import ArgumentParser
 
-import json
+OPT_VERBOSE = False
+
+class RPPDataObject():
+  def __init__(self, obj_type, **kvargs):
+    self.template_registry = {
+        'internal': {
+          'domain': 'models/internal/domain.json',
+          'contact': 'models/internal/contact.json',
+          'host': 'models/inernal/host.json',
+          },
+        'rpp': {
+          'domain': 'models/rpp/domain.json',
+          'contact': 'models/rpp/contact.json',
+          'host': 'models/rpp/host.json',
+        }
+      }
+
+
+    self.rpp_data_model = load_data_model(self.template_registry['rpp'][obj_type])
+    self.data = load_data_model(self.template_registry['internal'][obj_type])
+    if kvargs:
+      self.set_data(**kvargs)
+
+  def get_data(self):
+    return self.data
+
+  def set_data(self, **kvargs):
+    for k, v in kvargs.items():
+      self.data[k] = v
+
+  def __get_input(self, reference, value):
+    # assuming value is of the type we want to add
+    # e.g. 'string', ['string1', 'string2'], {'key1': 'string', 'key2', ['string1', 'string2']}
+    # FIXME: separate functions for str,dict,list
+    vprint("ask for {}, old value {}".format(reference, value))
+    if type(value) in (str, int, float):
+      user_input = input("{}({}):".format(reference, value))
+
+    elif type(value) == dict:
+      # iterate over sub-elements
+      user_input = {}
+      if input("Add element '{}' (Y/n)".format(reference)).lower() not in ('n', 'no'):
+        for k, v in value.items():
+          next_input = self.__get_input("{}->{}".format(reference, k), v)
+          if next_input:
+            user_input[k] = next_input
+
+    elif type(value) == list:
+      user_input = []
+      for i in range(1,10):
+        if type(value[0]) not in (str, int, float):
+          if input("Add element '{}' (Y/n)".format(reference)).lower() in ('n', 'no'):
+            break
+        next_input = self.__get_input("{}->{}".format(reference, i), value[0])
+        if next_input:
+          user_input.append(next_input)
+        else:
+          break
+
+    else:
+      print("Error: data type '{}' of '{}' is not supported yet")
+
+    if user_input:
+      return user_input
+    return None
+
+  def set_data_interactive(self):
+    for k, v in self.data.items():
+      user_input = self.__get_input(k, v)
+      if user_input:
+        self.data[k] = user_input
+    vprint("------- saved data -------")
+    vprint(json.dumps(self.data, indent=2))
+    vprint("------- saved data -------")
+
+
+  def get_rpp_data(self):
+    for k, v in self.data.items():
+      self.rpp_data_model[k] = v
+
+    for k, v in self.rpp_data_model.items():
+      if type(self.rpp_data_model[k]) == str and self.rpp_data_model[k] == "string":
+        self.rpp_data_model[k] = ""
+    return self.rpp_data_model
+
 
 
 class RppClient():
@@ -15,13 +98,20 @@ class RppClient():
 
   def parse_args(self):
     parser = ArgumentParser(description='Python RPP Client')
-    parser.add_argument('operation', choices=['create', 'info', 'check', 'update', 'delete'], help='Operation to perform')
-    parser.add_argument('-s', '--server', required=True, help='Base URL of the Server API', default='http://127.0.0.1:8000')
-    parser.add_argument('-d', '--domain', help='Domain (required for info, check, update, and delete operations)')
+    parser.add_argument('operation', help='Operation to perform', choices=['create', 'info', 'check', 'update', 'delete'])
+    parser.add_argument('-s', '--server', help='Base URL of the Server API', required=False, default='http://127.0.0.1:8000')
+    parser.add_argument('-d', '--domain', help='Domainname')
+    parser.add_argument('-i', '--interactive', help='Interactive template expansion', action='store_true')
+    parser.add_argument('-v', '--verbose', help='Verbose mode', action='store_true')
     #parser.add_argument('-f' '--file', default=None, required=False, help='JSON file containing the payload (required for create and update operations)')
     self.args = parser.parse_args()
 
+    if self.args.verbose:
+      global OPT_VERBOSE
+      OPT_VERBOSE = True
+
   def check_args(self):
+    # FIXME: currently ony domains, need to be fixed when implementing contacts or ...
     if self.args.operation in ['create', 'info', 'check', 'update', 'delete']:
       if not self.args.domain:
         print('Error: --domain is required for Domain ' + str(self.args.operation).upper() + ' operation')
@@ -49,10 +139,15 @@ class RppClient():
   def domain_create(self, domain=None):
     if domain:
       print('--- Domain CREATE (' + str(domain) + ') ---')
-      domain_data = load_data_model('examples/json/domain/domain.json')
+      domain = RPPDataObject("domain", name=domain)
+      if self.args.interactive:
+        domain.set_data_interactive()
+      else:
+        domain.set_data(name=domain)
+
+      domain_data = domain.get_rpp_data()
       if domain_data:
-        domain_data['name'] = domain
-        print(domain_data)
+        print(json.dumps(domain_data, indent=2))
         print(self._make_request('POST', '/domains', json=domain_data))
 
   def domain_info(self, domain=None):
@@ -95,9 +190,13 @@ def load_data_model(file_path):
         print(f'Error loading payload: {e}')
         return None
 
+def vprint(message):
+  if OPT_VERBOSE:
+    print(message)
+
 if __name__ == '__main__':
   rpp = RppClient()
-  print('\n-------------------------------------------------------------------------------------------------------------------\n'
+  vprint('\n-------------------------------------------------------------------------------------------------------------------\n'
         + str(rpp.args)
         + '\n-------------------------------------------------------------------------------------------------------------------'
     )
